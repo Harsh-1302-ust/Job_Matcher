ec62a2de011b715e92df6a6f6bedd9fdc8b683ea
Harsh-1302-ust <303368@ust.com>
2026-02-05 20:59:37 +0530

    changes

--- src/test.py
+++ src/test.py
@@ -2,4 +2,4 @@ from matcher.matcher import match_resumes
 
 if __name__ == '__main__':
     # non-interactive debug runner for job 'jd1' (show all candidates)
-    match_resumes('jd1', top_n=5, show_all=True)
+    match_resumes('JD_Data_Analyst', top_n=5, show_all=True)
